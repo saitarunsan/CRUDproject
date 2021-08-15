18cb1714e9896191f4b2394057b630eca1964275
sai tarun <saitarunsan@gmail.com>
2021-08-16 00:47:23 +0530

    created models and edit.html file

--- CRUDapp/models.py
+++ CRUDapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class ClientInfo(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField
+    phone = models.CharField(max_length=10)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = ("ClientInfo")
+        verbose_name_plural = ("ClientInfo")
+
+    def __str__(self):
+        return self.name
