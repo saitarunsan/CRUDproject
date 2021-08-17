c56c9f3782968137d209c8718c4323796f479123
sai tarun <saitarunsan@gmail.com>
2021-08-17 16:24:43 +0530

    updated email in models.py

--- CRUDapp/models.py
+++ CRUDapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class ClientInfo(models.Model):
     name = models.CharField(max_length=20)
-    email = models.EmailField
+    email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=10)
     status = models.BooleanField(default=True)
 
